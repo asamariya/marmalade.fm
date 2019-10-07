e7d8b0f478b58a195998a55ba5f565248b2f438d
Abhishek Samariya <asamariya@gmail.com>
2019-10-07 20:23:44 +1100

    import tachyons in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
 import './css/main.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
