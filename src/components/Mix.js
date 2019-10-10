2cad0dda603d5a84e6ff84eb5c63ff3c19831d32
Abhishek Samariya <asamariya@gmail.com>
2019-10-10 13:19:34 +1100

    Created button component

--- src/components/Mix.js
+++ src/components/Mix.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import PlayButton from './PlayButton';
+
 const Mix = props => (
 	<div className="aspect-ratio aspect-ratio--3x4 pointer bg-black">
 		<div className="ph3 pv4 aspect-ratio--object mix-overlay">
 			<div className="flex items-center relative z-2">
 				<h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{props.name}</h1>
 				{/* PlayButton goes here */}
+				<PlayButton />
 			</div>
 		</div>
 	</div>
---
+++ src/components/PlayButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayButton = () => (
+	<div>
+		<button
+			className="button bg-transparent contain button-reset play-button db 
+    bg-contain bg-center pointer center"
+		></button>
+	</div>
+);
+
+export default PlayButton;
