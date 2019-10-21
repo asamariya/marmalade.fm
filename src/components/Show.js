587b8d69210cf1aaa72ba9eba85a90b031da9f1e
Abhishek Samariya <asamariya@gmail.com>
2019-10-21 11:44:47 +1100

    Created tags on the show page

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,7 +1,27 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React, {Component} from 'react';
 import Stat from './Stat';
 
 import differenceInDays from 'date-fns/difference_in_days';
+
+const Tag = ({name, url}) => (
+	<div className="mr2 mb2 o-70">
+		<a
+			href={url}
+			className="block f6 link blue b ba bw1 b--blue br2 pv1 ph2 lh-title"
+			target="_blank"
+		>
+			{name}
+		</a>
+	</div>
+);
+const Tags = ({tags = []}) => (
+	<div className="tags flex flex-wrap">
+		{tags.map(tag => (
+			<Tag {...tag} />
+		))}
+	</div>
+);
 
 class Show extends Component {
 	render() {
@@ -11,6 +31,7 @@ class Show extends Component {
 		return (
 			<div className="ph3 ph4-l pad-bottom">
 				<div className="measure center lh-copy">
+					<Tags tags={mix.tags} />
 					<p>{mix.description}</p>
 					<Stat statName="plays" statNumber={mix.play_count} statWord="times" />
 					<Stat
