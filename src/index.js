19521e42b63e9d04a82bfd0ea243069ffd8ddf24
Abhishek Samariya <asamariya@gmail.com>
2019-10-21 13:15:51 +1100

    installed redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import 'tachyons';
 import './css/main.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import mixesApp from './store';
+
+let store = createStore(
+	mixesApp,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const Root = () => (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+	mixes: [],
+	currentMix: null
+};
+
+function mixesApp(state = initialState, action) {
+	return state;
+}
+
+export default mixesApp;
